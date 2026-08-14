4397bb68f43fdc497b6959db934de98a5199692a
asad222004 <rajputasadali22112004@gmail.com>
2026-08-14 16:00:32 +0500

    made the output look clean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # main.py file
-# accept the sequence
+# accept the DNA sequence from the user and call the functions from analyzer.py to perform the analysis
 from analyzer import  length
 from analyzer import  AGCTcontent
 from analyzer import  GCratio
@@ -7,29 +7,47 @@ from analyzer import  ATratio
 from analyzer import  complt
 from analyzer import  reverseCOMPLT
 from analyzer import  RNA
-seq = input("enter your DNA sequence here: ")
+
+seq = input("enter your DNA sequence here:\n ") 
+# accept the DNA sequence from the user
+
 seqNEW = seq.upper()
+ # changes the sequence to uppercase to avoid errors in the analysis
 
 Bases = ['A','G','C','T']
-valid = False
+valid = False 
+
+# flag to check if the sequence is valid
 for base in seqNEW:
     if base in Bases:
         valid = True
 if valid == True:
     seqLEN = length(seqNEW) 
-    print(f"The length of the sequence is: {seqLEN}")
+
+    print("---------------------------------\n")
+    print("    DNA Sequence Analysis")
+    print("---------------------------------\n")
+    #calling to different functions to perform the analysis and print the results
+    print(f"The length of the sequence is:\n=> {seqLEN}\n")
+    print("------------------------------------------\n")
     A,G,C,T= (AGCTcontent(seqNEW))
-    print(f"Adenine is: {A}\nGuanine is: {G}\nCytosine is: {C}\nThymine is: {T}")
+    print(f"Adenine is => {A}\nGuanine is => {G}\nCytosine is => {C}\nThymine is => {T}\n")
+    print("------------------------------------------\n")
     GCratio = GCratio(G,C, seqLEN)
-    print(f"The GC ratio is: {GCratio}")
+    print(f"The GC ratio is\n=> {GCratio}\n")
+    print("------------------------------------------\n")
     ATratio=ATratio(A, T, seqLEN)
-    print(f"The ATratio is :{ATratio}")
+    print(f"The ATratio is\n=> {ATratio}\n")
+    print("------------------------------------------\n")
     complement=complt(seqNEW)
-    print(f"The complement strand is:\n{complement}")
+    print(f"The complement strand is\n=>{complement}\n")
+    print("------------------------------------------\n")
     reverseComplement = reverseCOMPLT(seqNEW)
-    print(f"The reverse complement is:\n{reverseComplement}")
+    print(f"The reverse complement is:\n=>{reverseComplement}\n")
+    print("------------------------------------------\n")
     RNA = RNA(seqNEW)
-    print(f"Its RNA strand is:\n{RNA}")
+    print(f"Its RNA strand is:\n=>{RNA}\n")
+    print("------------------------------------------\n")
 
 else:
     print("Invalid DNA sequence. Please enter a valid sequence containing only A, G, C, and T.")
