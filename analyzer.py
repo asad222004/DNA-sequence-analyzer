58f04efcd0c949d895082147f18bd7362f8d148d
asad222004 <rajputasadali22112004@gmail.com>
2026-08-18 11:05:37 +0500

    made the code more readable

--- analyzer.py
+++ analyzer.py
@@ -1,18 +1,18 @@
 #cleaning the seqeuence to remove any invalid characters
 def clean(seqNEW):
-    cleaned_seq = ''
+    cleanSEQ = ''
     for base in seqNEW:
         if base in ['A', 'G', 'C', 'T']:
-            cleaned_seq += base
-    return cleaned_seq
+            cleanSEQ += base
+    return cleanSEQ
 
 # A function which calculates the content of A, G, C and T in a DNA sequence
-def AGCTcontent(seqNEW):
+def AGCTcontent(cleanSEQ):
     A = 0
     T = 0
     G = 0
     C = 0
-    for base in seqNEW:
+    for base in cleanSEQ:
         if base =='A':
             A+=1
         elif base =='G':
@@ -24,8 +24,8 @@ def AGCTcontent(seqNEW):
     return A,G,C,T         
 
 # function for length of the sequence
-def length(seqNEW):
-    seqLEN = len(seqNEW)
+def length(cleanSEQ):
+    seqLEN = len(cleanSEQ)
     return seqLEN 
 
 # Function for GC ratio calculation
@@ -39,9 +39,9 @@ def ATratio(A,T, seqLEN):
     return ATratio
 
 # Function for making complement
-def complt(seqNEW):
+def complt(cleanSEQ):
     complement = ""
-    for base in seqNEW:
+    for base in cleanSEQ:
         if base=='A':
             complement +='T'
         elif base == 'G':
@@ -53,13 +53,13 @@ def complt(seqNEW):
     return complement
 
 # function for converting to RNA
-def RNA(seqNEW):
-    RNA = seqNEW.replace('T' , 'U')
+def RNA(cleanSEQ):
+    RNA = cleanSEQ.replace('T' , 'U')
     return RNA
 
 # Function for reverse complement 
-def reverseCOMPLT(seqNEW):
-    newCOMPLT =seqNEW[::-1]    #this reverses the sequence
+def reverseCOMPLT(cleanSEQ):
+    newCOMPLT =cleanSEQ[::-1]    #this reverses the sequence
     reverseComplement= ""
     for base in newCOMPLT:
         if base=='A':
@@ -83,3 +83,42 @@ def ATskew(A,T):
     if A + T == 0:
         return 0
     return (A-T)/(A+T)
+
+#function to call after entering the sequence
+def call_analysis(seq):
+    print("---------------------------------\n")
+    print("    DNA Sequence Analysis")
+    print("---------------------------------\n")
+    seqNEW = seq.upper()
+    cleanSEQ = clean(seqNEW)
+    print(f"The cleaned sequence is:\n=> {cleanSEQ}\n")
+    print("------------------------------------------\n")
+    #calling to different functions to perform the analysis and print the results
+    seqLEN = length(cleanSEQ) 
+    print(f"The length of the sequence is:\n=> {seqLEN}\n")
+    print("------------------------------------------\n")
+    A,G,C,T= (AGCTcontent(cleanSEQ))
+    print(f"Adenine is => {A}\nGuanine is => {G}\nCytosine is => {C}\nThymine is => {T}\n")
+    print("------------------------------------------\n")
+    GC_ratio = GCratio(G,C, seqLEN)
+    print(f"The GC ratio is\n=> {GC_ratio}\n")
+    print("------------------------------------------\n")
+    GC_skew =GCskew(G,C)
+    print(f"The GC skew is\n=> {GC_skew}\n")
+    print("------------------------------------------\n")
+    AT_ratio = ATratio(A,T, seqLEN)
+    print(f"The AT ratio is\n=> {AT_ratio}\n")
+    print("------------------------------------------\n")
+    AT_skew = ATskew(A, T)
+    print(f"The AT skew is\n=> {AT_skew}\n")
+    print("------------------------------------------\n")
+    complement=complt(cleanSEQ)
+    print(f"The complement strand is\n=>{complement}\n")
+    print("------------------------------------------\n")
+    reverseComplement = reverseCOMPLT(cleanSEQ)
+    print(f"The reverse complement is:\n=>{reverseComplement}\n")
+    print("------------------------------------------\n")
+    RNAseq = RNA(cleanSEQ)
+    print(f"Its RNA strand is:\n=>{RNAseq}\n")
+    print("------------------------------------------\n")
+
--- main.py
+++ main.py
@@ -1,63 +1,7 @@
 # main.py file
 # accept the DNA sequence from the user and call the functions from analyzer.py to perform the analysis
-from analyzer import  length
-from analyzer import  AGCTcontent
-from analyzer import  GCratio
-from analyzer import  ATratio
-from analyzer import  complt
-from analyzer import  reverseCOMPLT
-from analyzer import  RNA
-from analyzer import  GCskew
-from analyzer import  ATskew
 
+from analyzer import  call_analysis
 seq = input("enter your DNA sequence here:\n ") 
-# accept the DNA sequence from the user
 
-seqNEW = seq.upper()
- # changes the sequence to uppercase to avoid errors in the analysis
-
-
-Bases = ['A','G','C','T']
-valid = False 
-
-# flag to check if the sequence is valid
-for base in seqNEW:
-    if base in Bases:
-        valid = True
-if valid == True:
-    
-
-    print("---------------------------------\n")
-    print("    DNA Sequence Analysis")
-    print("---------------------------------\n")
-    #calling to different functions to perform the analysis and print the results
-    seqLEN = length(seqNEW) 
-    print(f"The length of the sequence is:\n=> {seqLEN}\n")
-    print("------------------------------------------\n")
-    A,G,C,T= (AGCTcontent(seqNEW))
-    print(f"Adenine is => {A}\nGuanine is => {G}\nCytosine is => {C}\nThymine is => {T}\n")
-    print("------------------------------------------\n")
-    GCratio = GCratio(G,C, seqLEN)
-    print(f"The GC ratio is\n=> {GCratio}\n")
-    print("------------------------------------------\n")
-    GCskew =GCskew(G,C)
-    print(f"The GC skew is\n=> {GCskew}\n")
-    print("------------------------------------------\n")
-    ATratio = ATratio(A,T, seqLEN)
-    print(f"The AT ratio is\n=> {ATratio}\n")
-    print("------------------------------------------\n")
-    ATskew = ATskew(A, T)
-    print(f"The AT skew is\n=> {ATskew}\n")
-    print("------------------------------------------\n")
-    complement=complt(seqNEW)
-    print(f"The complement strand is\n=>{complement}\n")
-    print("------------------------------------------\n")
-    reverseComplement = reverseCOMPLT(seqNEW)
-    print(f"The reverse complement is:\n=>{reverseComplement}\n")
-    print("------------------------------------------\n")
-    RNA = RNA(seqNEW)
-    print(f"Its RNA strand is:\n=>{RNA}\n")
-    print("------------------------------------------\n")
-
-else:
-    print("The sequence is invalid. Please enter a valid DNA sequence containing only A, G, C, and T.")
+call_analysis(seq)
